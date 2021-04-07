eea95be468ff9e7c9bbac0337c733ea744e5371c
Trang <nguyenthitrang92519@gmail.com>
2021-04-07 12:10:52 +0700

    dien tich chu vi hinh tron

---
+++ dien_tich_hinh_tron.js
@@ -0,0 +1,9 @@
+let r=prompt('Nhap ban kinh hinh tron')
+const PI=3.14
+
+S=PI * r * r;
+document.write('Dien tich hinh tron la:'+S)
+document.write('<br/>')
+
+P=PI + 2*r;
+document.write('Chu vi hinh tron la:'+P)
