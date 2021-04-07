788377ae79002911416b846a16e49a89faeb14b0
Trang <nguyenthitrang92519@gmail.com>
2021-04-07 12:08:37 +0700

    tong diem hs

---
+++ bien_kieu_dl_toan_tu.js
@@ -0,0 +1,9 @@
+ Physics = +prompt('Enter the Physics')
+ Chemistry = +prompt('Enter the Chemictry')
+ Biology = +prompt('Enter the Biology')
+
+
+Average = (Physics + Chemistry + Biology)/3;
+alert("Average="+Average);
+Sum = Physics + Chemistry + Biology;
+alert("Sum="+Sum)
